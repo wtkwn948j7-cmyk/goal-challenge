124076e121fb3e8e140d4a0c79d77f5513ce92a4
wtkwn948j7-cmyk <wtkwn948j7@privaterelay.appleid.com>
2026-07-10 04:22:47 +0300

    Update players.js

--- players.js
+++ players.js
@@ -1,91 +1,57 @@
 const players = [
+
 {
-name: "Lionel Messi",
-club: 750,
-country: 112,
-ucl: 129,
-league: 496,
-image: "https://upload.wikimedia.org/wikipedia/commons/8/89/Portrait_Lionel_Messi.jpg"
+name:"Lionel Messi",
+image:"https://images.placeholders.dev/250x250?text=Messi",
+club:752,
+country:112,
+ucl:129,
+league:496
 },
 
 {
-name: "Cristiano Ronaldo",
-club: 790,
-country: 138,
-ucl: 140,
-league: 495,
-image: "https://upload.wikimedia.org/wikipedia/commons/8/8c/Cristiano_Ronaldo_2018.jpg"
+name:"Cristiano Ronaldo",
+image:"https://images.placeholders.dev/250x250?text=Ronaldo",
+club:791,
+country:138,
+ucl:140,
+league:539
 },
 
 {
-name: "Mohamed Salah",
-club: 290,
-country: 60,
-ucl: 51,
-league: 186,
-image: "https://upload.wikimedia.org/wikipedia/commons/8/8b/Mohamed_Salah_2018.jpg"
+name:"Mohamed Salah",
+image:"https://images.placeholders.dev/250x250?text=Salah",
+club:289,
+country:58,
+ucl:51,
+league:186
 },
 
 {
-name: "Erling Haaland",
-club: 320,
-country: 42,
-ucl: 49,
-league: 210,
-image: "https://upload.wikimedia.org/wikipedia/commons/6/6e/Erling_Haaland_2023.jpg"
+name:"Erling Haaland",
+image:"https://images.placeholders.dev/250x250?text=Haaland",
+club:321,
+country:42,
+ucl:49,
+league:180
 },
 
 {
-name: "Robert Lewandowski",
-club: 715,
-country: 85,
-ucl: 105,
-league: 487,
-image: "https://upload.wikimedia.org/wikipedia/commons/8/8e/Robert_Lewandowski_2020.jpg"
+name:"Robert Lewandowski",
+image:"https://images.placeholders.dev/250x250?text=Lewandowski",
+club:694,
+country:85,
+ucl:105,
+league:383
 },
 
 {
-name: "Kylian Mbappe",
-club: 300,
-country: 52,
-ucl: 55,
-league: 210,
-image: "https://upload.wikimedia.org/wikipedia/commons/8/8d/Kylian_Mbapp%C3%A9_2019.jpg"
-},
-
-{
-name: "Luis Suarez",
-club: 580,
-country: 69,
-ucl: 27,
-league: 380,
-image: "https://upload.wikimedia.org/wikipedia/commons/7/7d/Luis_Suarez_2018.jpg"
-},
-
-{
-name: "Neymar Jr",
-club: 450,
-country: 79,
-ucl: 43,
-league: 290,
-image: "https://upload.wikimedia.org/wikipedia/commons/5/5c/Neymar_2018.jpg"
-},
-
-{
-name: "Karim Benzema",
-club: 470,
-country: 37,
-ucl: 90,
-league: 281,
-image: "https://upload.wikimedia.org/wikipedia/commons/5/52/Karim_Benzema_2018.jpg"
-},
-
-{
-name: "Harry Kane",
-club: 390,
-country: 73,
-ucl: 35,
-league: 280,
-image: "https://upload.wikimedia.org/wikipedia/commons/5/5e/Harry_Kane_2021.jpg"
+name:"Kylian Mbappe",
+image:"https://images.placeholders.dev/250x250?text=Mbappe",
+club:352,
+country:52,
+ucl:55,
+league:221
 }
+
 ];
